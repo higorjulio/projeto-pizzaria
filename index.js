1afda4c8eb341695a4e15d3c8f37c7a3e0cd55d4
Magic302 <eqpminecraftiana@gmail.com>
2025-05-24 14:25:02 -0300

    Adicionado sistema de pedidos e algumas alterações minimas

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-document.addEventListener("DOMContentLoaded",() =>{
-    // adiciona logo a todas as paginas
-   let link = document.createElement("link");
-   link.rel = "shortcut icon";
-   link.href = "media/logo.png";
-   document.head.appendChild(link)
-
-
-})
 let botaotopo = document.getElementById("topo")
 window.addEventListener("scroll", () => {
     if(window.scrollY > 1000){
